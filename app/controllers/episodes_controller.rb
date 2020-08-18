f9c8201e8056e8b2d73b1e48bf9bf87bfdadcb98
Gabe-Corral <78gabe.corral@gmail.com>
2020-08-18 15:17:23 -0500

    submission for deadline

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,4 +3,12 @@ class EpisodesController < ApplicationController
   def index
     @episodes = Episode.all
   end
+
+  def show
+    @episode = Episode.find(params[:id])
+    @average_rating = @episode.appearances.map do |a|
+      a.rating
+    end
+  end
+
 end
